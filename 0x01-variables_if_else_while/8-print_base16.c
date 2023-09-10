403bf8ccbb49a3f8cb04dd6c8ab2343107adb13b
Heraman7 <heramanheraman8@gmail.com>
2023-09-10 09:53:20 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = '0';
+	d = 'a';
+	while
+		(c < 10) {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'f') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
