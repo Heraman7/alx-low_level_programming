458c9147027850e8310f4ed7bc2452987064a3cb
Heraman7 <heramanheraman8@gmail.com>
2023-09-13 08:14:44 -0700

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,9 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				_putchar("%d, ", i);
+				printf("%d, ", i);
 			else if (i == 98)
-				_putchar("%d\n", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
@@ -24,9 +24,9 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				_putchar("%d ", j);
+				printf("%d ", j);
 			else if (j == 98)
-				_putchar("%d\n", j);
+				printf("%d\n", j);
 		}
 	}
 
